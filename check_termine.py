97065c9ae7cff6fb3946288c959a4f89ad15964c
Saravanan Shanmugam <carbonleakage@outlook.com>
2022-07-14 22:17:55 +0200

    Changed to OOP, checking for anmeldung appointments

--- check_termine.py
+++ check_termine.py
@@ -1,83 +1,55 @@
-from urllib import response
+from types import NoneType
 import requests
 from bs4 import BeautifulSoup
 
 import logging
 
 logging.basicConfig(
-    filename="check_termine.log",
-    level=logging.DEBUG,
+    filename="check_anmeldung_termine.log",
+    level=logging.INFO,
     format="%(asctime)s --- %(message)s",
     encoding="latin",
 )
 
 
-def get_page(url: str) -> requests.Response:
-    """
-    Gets and returns the page content of url.
-    """
-    response = requests.get(url)
-    logging.info(f"Connect to url: {url}")
-    if response.status_code != 200:
-        logging.error(f"Received error code {response.status_code}!")
-    else:
-        logging.info("Successfully retrieved page!")
+class TerminePage():
 
-    return response
-
-
-def page_soup(response: requests.Response):
-
-    if response.status_code != 200:
-        soup = ""
-    else:
-        soup = BeautifulSoup(response.text, "html.parser")
-
-    return soup
-
-
-def check_buchbare_termine(url, months=[]):
-    """
-    Check the free appointments in the given berlin.de url
-    """
-    response = get_page(url)
-    soup = page_soup(response)
-
-    tables = soup.find_all("table")
-    try:
-        next = soup.find("th", class_="next").find("a", href=True)["href"]
-    except:
-        next = ""
-
-    for table in tables:
-        month = table.find("th", class_="month")
-        logging.debug(f"Found calendar month {month.text} in Termin webpage")
-        if month not in months:
-            months.append(month.text)
-            dates = table.find_all("td")
-            dates_notbookable = table.find_all("td", class_="nichtbuchbar")
-            dates_bookable = table.find_all("td", class_="buchbar")
-            logging.debug(f"Total number of dates in {month.text}: {len(dates)}")
-            if len(dates_bookable) > 0:
-                logging.debug(
-                    f"Number of bookable dates in {month.text}: __{len(dates_bookable)}__"
-                )
-            else:
-                logging.debug(
-                    f"Number of bookable dates in {month.text}: {len(dates_bookable)}"
-                )
-            logging.debug(
-                f"Number of not bookable dates in {month.text}: {len(dates_notbookable)}"
-            )
-    return months, next
-
+    def __init__(self, url, termine) -> None:
+        self.url = url
+        self.response = requests.get(self.url)
+        self.soup = BeautifulSoup(self.response.text, "html.parser")
+        self.next_page = "https://service.berlin.de" 
+        if next_page_link := self.soup.find("th", class_="next"): 
+            self.next_page += next_page_link.find("a", href=True)["href"]
+        self.termine = termine
+    
+    def count_frei_termine(self):
+        self._tables = self.soup.find_all("table")
+        for table in self._tables:
+            month = table.find("th", class_="month")
+            if month not in self.termine:
+                dates_notbookable = table.find_all("td", class_="nichtbuchbar")
+                dates_bookable = table.find_all("td", class_="buchbar")
+                self.termine[month.text, "bookable"] = len(dates_bookable)
+                self.termine[month.text, "not-available"] = len(dates_notbookable)
 
 if __name__ == "__main__":
-    logging.debug("-" * 20 + "Started running script!")
-    url = "https://service.berlin.de/terminvereinbarung/termin/tag.php?id=3061&anliegen[]=120691#&termin=1&dienstleister=327437&anliegen[]=120691&herkunft=1"
-    months = []
+    # Verpflictungserklärung
+    # url = "https://service.berlin.de/terminvereinbarung/termin/tag.php?id=3061&anliegen[]=120691#&termin=1&dienstleister=327437&anliegen[]=120691&herkunft=1"
+
+    logging.info("Started running script!") 
+    url = "https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleister=122276&anliegen[]=120686&herkunft=1"
+    termine = dict()
 
     while url != "https://service.berlin.de":
-        months, url = check_buchbare_termine(url, months)
-        url = "https://service.berlin.de" + url
-    logging.debug("-" * 20 + "Finished running script!")
+        logging.info(f"Checking appointments in {url}") 
+        page = TerminePage(url, termine)
+        page.count_frei_termine()
+        url = page.next_page
+        termine = page.termine
+    
+    for month, termine_type in termine:
+        logging.info(
+            f"Numer of {termine_type} dates in {month}: {termine[month, termine_type]}"
+        )
+    logging.info("Finished running script!") 
